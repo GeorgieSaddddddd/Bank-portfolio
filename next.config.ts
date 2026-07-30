7a3daafaf273d5f3eac5bba3e813d9585511b487
Sirichok Leelathawornkun <SirichokBank77@gmail.com>
2026-07-31 02:59:58 +0700

    Prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export', // จำเป็นสำหรับการเอาไปลง GitHub Pages
-  basePath: isProd ? '/Bank-portfolio' : '', // <-- เปลี่ยนเป็นชื่อโปรเจกต์ใหม่ (ตัวพิมพ์เล็ก/ใหญ่ต้องเป๊ะ)
-  assetPrefix: isProd ? '/Bank-portfolio/' : '',
+  output: 'export',
+  basePath: '/Bank-portfolio',   
+  assetPrefix: '/Bank-portfolio/', 
   images: {
-    unoptimized: true, // GitHub Pages ไม่รองรับ Image Optimization
+    unoptimized: true,
   },
 };
 
